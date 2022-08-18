474f1c8147d129974f0e3718dc272eff1f6db73a
midlynj <jmidlyn@gmail.com>
2022-08-18 11:17:05 -0500

    constructor and getters created

--- src/contacts/Contacts.java
+++ src/contacts/Contacts.java
@@ -1,4 +1,27 @@
 package contacts;
 
 public class Contacts {
+    public String contactName;
+    private int phoneNumber;
+
+    public Contacts(String contactName, int phoneNumber) {
+        this.contactName = contactName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
